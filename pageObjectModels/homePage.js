329f0de7917090c03d673642c8b1a9ca5f6c5d1e
cata-mateas16 <catalin.mateas@softgenius.com>
2024-07-18 13:37:28 +0300

    chat modal test

--- pageObjectModels/homePage.js
+++ pageObjectModels/homePage.js
@@ -17,12 +17,28 @@ export class HomePage {
     this.languageSelectText = page.getByText('Selectează Limba');
     this.closeLanguageModal = page.locator('#cdk-overlay-0').getByRole('button').first()
 
+    /* chat modal */
+    this.chatModal = page.getByRole('button', { name: 'logo' });
+    this.chatModalTitle = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByTestId('widget-title');
+    this.welcomeMessage = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByText('Welcome to Customer Support!');
+    this.nameLabel = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').locator('label').filter({ hasText: 'Nume' }).locator('div');
+    this.emailLabel = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').locator('label').filter({ hasText: 'E-mail' }).locator('div');
+    this.messageLabel = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').locator('label').filter({ hasText: 'Mesaj' }).locator('div');
+    this.startConversatioButton = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByTestId('button-ok');
+    this.nameTextBox = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByLabel('Nume');
+    this.emailTextBox = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByLabel('E-mail');
+    this.messageTextBox = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByLabel('Mesaj');
+    this.closeChatModalButton = page.frameLocator('iframe[title="Găsiți mai multe informații aici"]').getByLabel('Minimizare miniaplicație');
   }
 
   async checkCasinoTitle() {
     if(process.env.BASE_URL.includes('don')) {
       return 'Descoperă pariurile online și jocurile de cazinou online la DonRO';
     }
+  }
+
+  async openChatModal() {
+    await this.chatModal.click();
   }
 
 }
--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -20,9 +20,33 @@ test.afterAll(async () => {
 });
 
 test.describe('user NOT logged in', () => {
+  // eslint-disable-next-line no-unused-vars
   test('verify that casino title is visible and correct based on Casino', async ({ isMobile }) => {
     const expectedTitle = await homePage.checkCasinoTitle();
     await expect(page).toHaveTitle(expectedTitle);
   });
+
+  // eslint-disable-next-line no-unused-vars
+  test('verify that change language button is displayed', async ({isMobile}) => {
+    await expect(homePage.switchLanguageModal).toBeVisible();
+  });
   
+  // eslint-disable-next-line no-unused-vars
+  test('verify that chat button is displayed and its elements are visible', async ({isMobile}) => {
+    await expect(homePage.chatModal).toBeVisible();
+
+    await homePage.openChatModal();
+
+    await expect(homePage.chatModalTitle).toBeVisible;
+    await expect(homePage.welcomeMessage).toBeVisible;
+    await expect(homePage.nameLabel).toBeVisible;
+    await expect(homePage.emailLabel).toBeVisible;
+    await expect(homePage.messageLabel).toBeVisible;
+    await expect(homePage.startConversationButton).toBeVisible;
+    await expect(homePage.nameTextBox).toBeVisible;
+    await expect(homePage.emailTextBox).toBeVisible;
+    await expect(homePage.messageTextBox).toBeVisible;
+    await expect(homePage.closeChatModuleButton).toBeVisible;
+  });
+
 })
